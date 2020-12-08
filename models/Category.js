94e8c298d57819eb42faed6456f051b5138f7176
henry <henry@0216.com>
2020-12-08 17:28:30 +0800

    新增分类接口

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
     name:{type:String},
-    url:{type:String}
+    url:{type:String},
+    classId:{type:mongoose.SchemaTypes.ObjectId,ref:'Classification'},
+    describe:{type:String},
 })
 module.exports = mongoose.model('Category',schema)
